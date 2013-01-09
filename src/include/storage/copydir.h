f2efda27e4bb7722f0a4b2386c5bddba6e8366c3
Andres Freund <andres@anarazel.de>
2013-01-09 17:36:20 +0100

    wal_decoding: copydir: make fsync_fname public
    This probably should be somewhere else, its a generally useful function, not
    really related to copying directories. fd.[ch]?

--- src/include/storage/copydir.h
+++ src/include/storage/copydir.h
@@ -15,5 +15,6 @@
 
 extern void copydir(char *fromdir, char *todir, bool recurse);
 extern void copy_file(char *fromfile, char *tofile);
+extern void fsync_fname(char *fname, bool isdir);
 
 #endif   /* COPYDIR_H */
