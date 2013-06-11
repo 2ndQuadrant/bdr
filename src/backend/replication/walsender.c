1068749d3e689051068b1c98b48778760355b719
Abhijit Menon-Sen <ams@2ndQuadrant.com>
2013-06-11 23:25:26 +0200

    wal_decoding: Add pg_xlog_wait_remote_{apply,receive} functions
    We want to use these in isolationtester tests, but they're more
    generally useful for "inter-node synchronisation".

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -2970,3 +2970,80 @@ GetOldestWALSendPointer(void)
 }
 
 #endif
+
+static XLogRecPtr
+text_to_xlogrecptr(text *str)
+{
+	uint32 hi, lo;
+	char *pos = text_to_cstring(str);
+
+	if (sscanf(pos, "%X/%X", &hi, &lo) != 2)
+        ereport(ERROR,
+                (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                 errmsg("could not parse transaction log location \"%s\"",
+                        pos)));
+
+	return ((uint64) hi) << 32 | lo;
+}
+
+static void
+wait_for_remote_lsn(int32 pid, XLogRecPtr ptr, bool wait_for_apply)
+{
+	int i;
+	bool done;
+
+	do {
+		done = true;
+
+		for (i = 0; i < max_wal_senders; i++)
+		{
+			volatile WalSnd *walsnd = &WalSndCtl->walsnds[i];
+
+			SpinLockAcquire(&walsnd->mutex);
+
+			if (walsnd->pid != 0 && (pid == 0 || pid == walsnd->pid))
+			{
+				XLogRecPtr rptr = wait_for_apply ? walsnd->apply : walsnd->flush;
+				if (rptr < ptr)
+					done = false;
+			}
+
+			SpinLockRelease(&walsnd->mutex);
+
+			if (!done)
+				break;
+		}
+
+		if (!done)
+			pg_usleep(10*1000);
+	}
+	while (!done);
+}
+
+Datum
+pg_xlog_wait_remote_apply(PG_FUNCTION_ARGS)
+{
+	text *pos = PG_GETARG_TEXT_P(0);
+	int32 pid = PG_GETARG_INT32(1);
+
+	XLogRecPtr startpos = text_to_xlogrecptr(pos);
+
+	if (GetTopTransactionIdIfAny() != InvalidTransactionId)
+		elog(ERROR, "waiting in xact with dml");
+
+	wait_for_remote_lsn(pid, startpos, true);
+
+	PG_RETURN_VOID();
+}
+
+Datum
+pg_xlog_wait_remote_receive(PG_FUNCTION_ARGS)
+{
+	text *pos = PG_GETARG_TEXT_P(0);
+	int32 pid = PG_GETARG_INT32(1);
+
+	XLogRecPtr startpos = text_to_xlogrecptr(pos);
+	wait_for_remote_lsn(pid, startpos, false);
+
+	PG_RETURN_VOID();
+}
