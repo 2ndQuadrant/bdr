96f2ccbeca5622c98ebe095db4c6f145088948a8
Alvaro Herrera <alvherre@alvh.no-ip.org>
2014-02-11 15:58:56 -0300

    core: split builtins.h to new include/utils/ruleutils.h
    This contains many prototypes for functions in ruleutils.c that are not
    exposed to the SQL level.

--- src/backend/utils/adt/ruleutils.c
+++ src/backend/utils/adt/ruleutils.c
@@ -54,6 +54,7 @@
 #include "utils/fmgroids.h"
 #include "utils/lsyscache.h"
 #include "utils/rel.h"
+#include "utils/ruleutils.h"
 #include "utils/snapmgr.h"
 #include "utils/syscache.h"
 #include "utils/tqual.h"
