eaa703dd2f265f769b213c37ef04480becc8833c
Craig Ringer <craig@2ndquadrant.com>
2014-04-08 13:03:28 +0800

    bdr: Split common connection setup routines out of bdr_apply_main

--- contrib/bdr/bdr.c
+++ contrib/bdr/bdr.c
@@ -199,16 +199,36 @@ process_remote_action(StringInfo s)
 	}
 }
 
-static void
-bdr_apply_main(Datum main_arg)
+/*
+ * Establish a BDR connection
+ *
+ * Connects to the remote node, identifies it, and generates local
+ * and remote replication identifiers and slot name.
+ *
+ * The local replication identifier is not saved, the caller must do that.
+ *
+ * Returns the PGconn for the established connection.
+ *
+ * Sets out parameters:
+ *   remote_ident
+ *   slot_name
+ *   remote_sysid_i
+ *   remote_tlid_i
+ */
+static PGconn*
+bdr_connect(
+	char *conninfo_repl,
+	char* remote_ident, size_t remote_ident_length,
+	NameData* slot_name,
+	uint64* remote_sysid_i,
+	TimeLineID *remote_tlid_i
+	)
 {
 	PGconn	   *streamConn;
 	PGresult   *res;
-	int			fd;
+	StringInfoData query;
 	char	   *remote_sysid;
-	uint64		remote_sysid_i;
 	char	   *remote_tlid;
-	TimeLineID	remote_tlid_i;
 
 #ifdef NOT_USED
 	char	   *remote_dbname;
@@ -216,12 +236,94 @@ bdr_apply_main(Datum main_arg)
 	char	   *remote_dboid;
 	Oid			remote_dboid_i;
 	char		local_sysid[32];
+	NameData	replication_name;
+
+	initStringInfo(&query);
+	NameStr(replication_name)[0] = '\0';
+
+	streamConn = PQconnectdb(conninfo_repl);
+	if (PQstatus(streamConn) != CONNECTION_OK)
+	{
+		ereport(FATAL,
+				(errmsg("could not connect to the primary server: %s",
+						PQerrorMessage(streamConn))));
+	}
+
+	res = PQexec(streamConn, "IDENTIFY_SYSTEM");
+	if (PQresultStatus(res) != PGRES_TUPLES_OK)
+	{
+		elog(FATAL, "could not send replication command \"%s\": %s",
+			 "IDENTIFY_SYSTEM", PQerrorMessage(streamConn));
+	}
+	if (PQntuples(res) != 1 || PQnfields(res) != 5)
+	{
+		elog(FATAL, "could not identify system: got %d rows and %d fields, expected %d rows and %d fields\n",
+			 PQntuples(res), PQnfields(res), 1, 5);
+	}
+
+	remote_sysid = PQgetvalue(res, 0, 0);
+	remote_tlid = PQgetvalue(res, 0, 1);
+#ifdef NOT_USED
+	remote_dbname = PQgetvalue(res, 0, 3);
+#endif
+	remote_dboid = PQgetvalue(res, 0, 4);
+
+	if (sscanf(remote_sysid, UINT64_FORMAT, remote_sysid_i) != 1)
+		elog(ERROR, "could not parse remote sysid %s", remote_sysid);
+
+	if (sscanf(remote_tlid, "%u", remote_tlid_i) != 1)
+		elog(ERROR, "could not parse remote tlid %s", remote_tlid);
+
+	if (sscanf(remote_dboid, "%u", &remote_dboid_i) != 1)
+		elog(ERROR, "could not parse remote database OID %s", remote_dboid);
+
+	snprintf(local_sysid, sizeof(local_sysid), UINT64_FORMAT,
+			 GetSystemIdentifier());
+
+	if (strcmp(remote_sysid, local_sysid) == 0)
+	{
+		ereport(FATAL,
+				(errmsg("system identifiers must differ between the nodes"),
+				 errdetail("Both system identifiers are %s.", remote_sysid)));
+	}
+	else
+		elog(LOG, "local sysid %s, remote: %s",
+			 local_sysid, remote_sysid);
+
+	/*
+	 * build slot name.
+	 *
+	 * FIXME: This might truncate the identifier if replication_name is
+	 * somewhat longer...
+	 */
+	snprintf(NameStr(*slot_name), NAMEDATALEN, "bdr_%u_%s_%u_%u__%s",
+			 remote_dboid_i, local_sysid, ThisTimeLineID,
+			 MyDatabaseId, NameStr(replication_name));
+	NameStr(*slot_name)[NAMEDATALEN - 1] = '\0';
+
+	/*
+	 * Build replication identifier.
+	 */
+	snprintf(remote_ident, remote_ident_length, "bdr_"UINT64_FORMAT"_%u_%u_%u_%s",
+			 *remote_sysid_i, *remote_tlid_i, remote_dboid_i, MyDatabaseId, NameStr(replication_name));
+
+	/* no parts of IDENTIFY_SYSTEM's response needed anymore */
+	PQclear(res);
+
+	return streamConn;
+}
+
+static void
+bdr_apply_main(Datum main_arg)
+{
+	PGconn	   *streamConn;
+	PGresult   *res;
+	int			fd;
 	char		remote_ident[256];
 	StringInfoData query;
 	char		conninfo_repl[MAXCONNINFO + 75];
 	XLogRecPtr	last_received = InvalidXLogRecPtr;
 	char	   *sqlstate;
-	NameData	replication_name;
 	RepNodeId	replication_identifier;
 	XLogRecPtr	start_from;
 	NameData	slot_name;
@@ -229,8 +331,6 @@ bdr_apply_main(Datum main_arg)
 	initStringInfo(&query);
 
 	bdr_apply_con = (BDRWorkerCon *) DatumGetPointer(main_arg);
-
-	NameStr(replication_name)[0] = '\0';
 
 	/* Establish signal handlers before unblocking signals. */
 	pqsignal(SIGHUP, bdr_sighup);
@@ -264,71 +364,13 @@ bdr_apply_main(Datum main_arg)
 	elog(LOG, "%s initialized on %s, remote %s",
 		 MyBgworkerEntry->bgw_name, bdr_apply_con->dbname, conninfo_repl);
 
-	streamConn = PQconnectdb(conninfo_repl);
-	if (PQstatus(streamConn) != CONNECTION_OK)
-	{
-		ereport(FATAL,
-				(errmsg("could not connect to the primary server: %s",
-						PQerrorMessage(streamConn))));
-	}
-
-	res = PQexec(streamConn, "IDENTIFY_SYSTEM");
-	if (PQresultStatus(res) != PGRES_TUPLES_OK)
-	{
-		elog(FATAL, "could not send replication command \"%s\": %s",
-			 "IDENTIFY_SYSTEM", PQerrorMessage(streamConn));
-	}
-	if (PQntuples(res) != 1 || PQnfields(res) != 5)
-	{
-		elog(FATAL, "could not identify system: got %d rows and %d fields, expected %d rows and %d fields\n",
-			 PQntuples(res), PQnfields(res), 1, 5);
-	}
-
-	remote_sysid = PQgetvalue(res, 0, 0);
-	remote_tlid = PQgetvalue(res, 0, 1);
-#ifdef NOT_USED
-	remote_dbname = PQgetvalue(res, 0, 3);
-#endif
-	remote_dboid = PQgetvalue(res, 0, 4);
-
-	if (sscanf(remote_sysid, UINT64_FORMAT, &remote_sysid_i) != 1)
-		elog(ERROR, "could not parse remote sysid %s", remote_sysid);
-
-	if (sscanf(remote_tlid, "%u", &remote_tlid_i) != 1)
-		elog(ERROR, "could not parse remote tlid %s", remote_tlid);
-
-	if (sscanf(remote_dboid, "%u", &remote_dboid_i) != 1)
-		elog(ERROR, "could not parse remote database OID %s", remote_dboid);
-
-	snprintf(local_sysid, sizeof(local_sysid), UINT64_FORMAT,
-			 GetSystemIdentifier());
-
-	if (strcmp(remote_sysid, local_sysid) == 0)
-	{
-		ereport(FATAL,
-				(errmsg("system identifiers must differ between the nodes"),
-				 errdetail("Both system identifiers are %s.", remote_sysid)));
-	}
-	else
-		elog(LOG, "local sysid %s, remote: %s",
-			 local_sysid, remote_sysid);
-
-	/*
-	 * build slot name.
-	 *
-	 * FIXME: This might truncate the identifier if replication_name is
-	 * somewhat longer...
-	 */
-	snprintf(NameStr(slot_name), NAMEDATALEN, "bdr_%u_%s_%u_%u__%s",
-			 remote_dboid_i, local_sysid, ThisTimeLineID,
-			 MyDatabaseId, NameStr(replication_name));
-	NameStr(slot_name)[NAMEDATALEN - 1] = '\0';
-
-	/*
-	 * Build replication identifier.
-	 */
-	snprintf(remote_ident, sizeof(remote_ident), "bdr_"UINT64_FORMAT"_%u_%u_%u_%s",
-			 remote_sysid_i, remote_tlid_i, remote_dboid_i, MyDatabaseId, NameStr(replication_name));
+	streamConn = bdr_connect(
+		conninfo_repl,
+		remote_ident, sizeof(remote_ident),
+		&slot_name,
+		&bdr_apply_con->sysid,
+		&bdr_apply_con->timeline
+		);
 
 	StartTransactionCommand();
 
@@ -336,8 +378,6 @@ bdr_apply_main(Datum main_arg)
 
 	CommitTransactionCommand();
 
-	/* no parts of IDENTIFY_SYSTEM's response needed anymore */
-	PQclear(res);
 
 	if (OidIsValid(replication_identifier))
 		elog(LOG, "found valid replication identifier %u", replication_identifier);
@@ -379,8 +419,6 @@ bdr_apply_main(Datum main_arg)
 	}
 
 	bdr_apply_con->origin_id = replication_identifier;
-	bdr_apply_con->sysid = remote_sysid_i;
-	bdr_apply_con->timeline = remote_tlid_i;
 
 	/* initialize stat subsystem, our id won't change further */
 	bdr_count_set_current_node(replication_identifier);
@@ -709,6 +747,28 @@ bdr_static_worker(Datum main_arg)
 
 	bdr_static_con = (BDRStaticCon *) DatumGetPointer(main_arg);
 
+	/* Establish signal handlers before unblocking signals. */
+	pqsignal(SIGHUP, bdr_sighup);
+	pqsignal(SIGTERM, bdr_sigterm);
+
+	/* We're now ready to receive signals */
+	BackgroundWorkerUnblockSignals();
+
+	/* Connect to our database */
+	BackgroundWorkerInitializeConnection(bdr_static_con->dbname, NULL);
+
+	/* make sure BDR extension exists */
+	bdr_maintain_schema();
+
+	/* always work in our own schema */
+	SetConfigOption("search_path", "bdr, pg_catalog",
+					PGC_BACKEND, PGC_S_OVERRIDE);
+
+	CurrentResourceOwner = ResourceOwnerCreate(NULL, "bdr seq top-level resource owner");
+	bdr_saved_resowner = CurrentResourceOwner;
+
+	elog(LOG, "BDR starting sequencer on db \"%s\"", bdr_static_con->dbname);
+
 	elog(LOG, "Starting bdr worker for %s", bdr_static_con->dbname);
 
 	/* Common apply worker values */
@@ -738,28 +798,6 @@ bdr_static_worker(Datum main_arg)
 		RegisterDynamicBackgroundWorker(&apply_worker, &bgw_handle);
 		apply_workers = lcons(bgw_handle, apply_workers);
 	}
-
-	/* Establish signal handlers before unblocking signals. */
-	pqsignal(SIGHUP, bdr_sighup);
-	pqsignal(SIGTERM, bdr_sigterm);
-
-	/* We're now ready to receive signals */
-	BackgroundWorkerUnblockSignals();
-
-	/* Connect to our database */
-	BackgroundWorkerInitializeConnection(bdr_static_con->dbname, NULL);
-
-	/* make sure BDR extension exists */
-	bdr_maintain_schema();
-
-	/* always work in our own schema */
-	SetConfigOption("search_path", "bdr, pg_catalog",
-					PGC_BACKEND, PGC_S_OVERRIDE);
-
-	CurrentResourceOwner = ResourceOwnerCreate(NULL, "bdr seq top-level resource owner");
-	bdr_saved_resowner = CurrentResourceOwner;
-
-	elog(LOG, "BDR starting sequencer on db \"%s\"", bdr_static_con->dbname);
 
 	/* initialize sequencer */
 	bdr_sequencer_init();
