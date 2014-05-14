92c6b099f131ee952cf0fa9d17465d3e01e2097c
Craig Ringer <craig@2ndquadrant.com>
2014-05-14 09:47:39 +0800

    bdr: Prevent slot creation until BDR is up and ready
    Checks bdr.bdr_nodes status field for the local node in the local DB during bdr
    output plugin startup and ERRORs if status isn't set to ready yet. This ensures
    that we cannot begin sending changes to other nodes while we're still applying
    a dump or performing catchup.
    
    The BDR per-db worker now creates an entry in the local bdr.bdr_nodes if
    init_replica isn't configured on any connection and no such entry exists. That
    makes sure BDR without init_replica keeps working properly.
    
    An error like:
    
        ERROR:  bdr.bdr_nodes entry for local node (sysid=6013159507840554182, dbname=postgres): row missing, bdr not active on this database or is initializing.
    
    is emitted if the DB isn't ready for BDR yet. This can be tested easily with
    pg_recvlogical by deleting the bdr.bdr_nodes entry for the local node or
    creating an init_replica script that pauses indefinitely.

--- contrib/bdr/worker.mk
+++ contrib/bdr/worker.mk
@@ -3,7 +3,7 @@
 MODULE_big = bdr
 OBJS = bdr.o bdr_apply.o bdr_compat.o bdr_commandfilter.o bdr_count.o \
 	bdr_seq.o bdr_init_replica.o bdr_relcache.o bdr_conflict_handlers.o \
-	bdr_conflict_logging.o bdr_executor.o
+	bdr_conflict_logging.o bdr_executor.o bdr_catalogs.o
 
 EXTENSION = bdr
 DATA = bdr--0.5.sql
