1b61952833c835ed8965c29061a843ddb41b260f
Andres Freund <andres@anarazel.de>
2014-07-18 12:19:33 +0200

    bdr: Add regression tests for CREATE/DROP extension replication.

---
+++ sql/ddl/extension.sql
@@ -0,0 +1,22 @@
+\c postgres
+-- create nonexistant extension
+CREATE EXTENSION pg_trgm;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location()::text, pid) FROM pg_stat_replication;
+\c regression
+\dx pg_trgm
+
+-- drop and recreate using CINE
+DROP EXTENSION pg_trgm;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location()::text, pid) FROM pg_stat_replication;
+\c postgres
+\dx pg_trgm
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location()::text, pid) FROM pg_stat_replication;
+\c regression
+\dx pg_trgm
+
+-- CINE existing extension
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location()::text, pid) FROM pg_stat_replication;
+\c postgres
+\dx pg_trgm
