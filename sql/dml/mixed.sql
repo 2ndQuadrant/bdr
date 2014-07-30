729cd82694c9707b0be997c4b7424829580a0a0c
Petr Jelinek <pjmodos@pjmodos.net>
2014-07-31 01:31:52 +0200

    bdr: fix old tuple logging after table structure change.
    The tuple_to_stringinfo function which used for logging tuple contents
    was calling fastgetattr to get value of an attribute, but fastgetattr
    does not work correctly on tuples that were written before table
    structure change (column is added or dropped). Fix is to use
    heap_getattr.
    
    Regression test is attached as well.
    
    Per report by Keaton Adams (RT-#37869)

---
+++ sql/dml/mixed.sql
@@ -0,0 +1,21 @@
+-- test for RT-#37869
+
+CREATE TABLE add_column (
+    id serial primary key,
+    data text
+);
+
+INSERT INTO add_column (data) SELECT generate_series(1,100,10);
+
+ALTER TABLE add_column ADD COLUMN other varchar(100);
+
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location()::text, pid) FROM pg_stat_replication;
+\c postgres
+SELECT id, data, other FROM add_column ORDER BY id;
+
+UPDATE add_column SET other = 'foobar';
+
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location()::text, pid) FROM pg_stat_replication;
+\c regression
+SELECT id, data, other FROM add_column ORDER BY id;
+
