04662549cc6cf4d852c5a52cee2bfecfa3b81c63
Petr Jelinek <pjmodos@pjmodos.net>
2014-08-29 16:58:22 +0200

    bdr: fix the possibly broken^Conflict history sequence in upgrade script
    See 766e3bcb3c3c37981b12f93d0e4210213fa281c2 for more info.

--- contrib/bdr/bdr--0.7--0.7.1.sql
+++ contrib/bdr/bdr--0.7--0.7.1.sql
@@ -4,3 +4,10 @@ DO $$BEGIN
 EXCEPTION WHEN duplicate_function THEN
 	RETURN;
 END$$;
+
+BEGIN;
+SET LOCAL bdr.skip_ddl_replication = true;
+SET LOCAL bdr.permit_unsafe_ddl_commands = true;
+ALTER SEQUENCE bdr.bdr_conflict_history_id_seq USING local;
+SELECT setval('bdr.bdr_conflict_history_id_seq', (SELECT coalesce(max(conflict_id)+1,1) FROM bdr.bdr_conflict_history));
+COMMIT;
