ace6160c6836e63fb777fad2c05879aaa816c8c6
Craig Ringer <craig@2ndquadrant.com>
2014-11-25 16:20:14 +0800

    bdr: Enforce only one init_replica connection per node in catalog
    A partial unique index on bdr.bdr_connections ensures that
    only one init_replica enabled connection per node may exist.

--- bdr--0.8.0.5--0.8.0.6.sql
+++ bdr--0.8.0.5--0.8.0.6.sql
@@ -9,7 +9,7 @@ CREATE TABLE bdr_connections (
     conn_timeline oid not null, -- Ditto
     conn_dboid oid not null,  -- This is an oid local to the node_sysid cluster
     conn_local_name name not null
-	CHECK(conn_local_name <> ''),
+        CHECK(conn_local_name <> ''),
     PRIMARY KEY(conn_sysid, conn_timeline, conn_dboid, conn_local_name),
     -- Wondering why there's no FOREIGN KEY to bdr.bdr_nodes?
     -- It won't be populated until after the connection exists.
@@ -18,23 +18,27 @@ CREATE TABLE bdr_connections (
     remote_timeline oid,
     remote_dboid oid,
     CONSTRAINT remote_ids_all_null_or_all_non_null
-	CHECK(remote_sysid IS NULL AND remote_timeline IS NULL AND remote_dboid IS NULL
-	   OR remote_sysid IS NOT NULL AND remote_timeline IS NOT NULL AND remote_dboid IS NOT NULL),
+        CHECK(remote_sysid IS NULL AND remote_timeline IS NULL AND remote_dboid IS NULL
+           OR remote_sysid IS NOT NULL AND remote_timeline IS NOT NULL AND remote_dboid IS NOT NULL),
 
     conn_replication_name name not null,
     CONSTRAINT conn_replication_name_is_unused
-	CHECK(conn_replication_name = ''), -- We don't currently support replication_name on the wire
+        CHECK(conn_replication_name = ''), -- We don't currently support replication_name on the wire
 
     conn_dsn text not null,
     conn_init_replica boolean not null default false,
     conn_replica_local_dsn text,
     CHECK(conn_replica_local_dsn IS NOT NULL = conn_init_replica),
     conn_apply_delay integer
-	CHECK (conn_apply_delay >= 0),
+        CHECK (conn_apply_delay >= 0),
     conn_replication_sets text[]
 );
 
 REVOKE ALL ON TABLE bdr_connections FROM public;
+
+CREATE UNIQUE INDEX bdr_connections_only_one_init_replica_conn_per_node
+ON bdr_connections(conn_sysid, conn_timeline, conn_dboid)
+WHERE (conn_init_replica);
 
 COMMENT ON TABLE bdr_connections IS 'Connections to upstream BDR nodes from this node. Don''t modify this directly, use the provided functions.';
 
@@ -56,16 +60,12 @@ COMMENT ON COLUMN bdr_connections.conn_replication_sets IS 'Replication sets thi
 -- because this is a global table that's sync'd between nodes.
 
 
--- Data structures for BDR's dynamic configuration management
-
-SET LOCAL search_path = bdr;
-SET bdr.permit_unsafe_ddl_commands = true;
-SET bdr.skip_ddl_replication = true;
-
 CREATE FUNCTION bdr_get_local_nodeid( sysid OUT oid, timeline OUT oid, dboid OUT oid)
 RETURNS record LANGUAGE c AS 'MODULE_PATHNAME';
 
 -- bdr_get_local_nodeid is intentionally not revoked from all, it's read-only
+
+
 
 CREATE FUNCTION bdr_start_perdb_worker()
 RETURNS void LANGUAGE c AS 'MODULE_PATHNAME';
@@ -73,6 +73,7 @@ RETURNS void LANGUAGE c AS 'MODULE_PATHNAME';
 REVOKE ALL ON FUNCTION bdr_start_perdb_worker() FROM public;
 
 COMMENT ON FUNCTION bdr_start_perdb_worker() IS 'Internal BDR function, do not call directly.';
+
 
 RESET bdr.permit_unsafe_ddl_commands;
 RESET bdr.skip_ddl_replication;
