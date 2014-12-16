3b8da665d1c816e94714939cd3c02db1ca430dc1
Ian Barwick <ian@2ndquadrant.com>
2014-12-16 20:14:11 +0900

    Add CREATE TYPE basic test

---
+++ src/test/regress/ddl-deparse/sql/create_type.sql
@@ -0,0 +1,22 @@
+---
+--- CREATE_TYPE
+---
+
+CREATE FUNCTION text_w_default_in(cstring)
+   RETURNS text_w_default
+   AS 'textin'
+   LANGUAGE internal STABLE STRICT;
+
+CREATE FUNCTION text_w_default_out(text_w_default)
+   RETURNS cstring
+   AS 'textout'
+   LANGUAGE internal STABLE STRICT ;
+
+CREATE TYPE text_w_default (
+   internallength = variable,
+   input = text_w_default_in,
+   output = text_w_default_out,
+   alignment = int4,
+   default = 'foo',
+   collatable = TRUE
+);
