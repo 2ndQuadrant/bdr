ef7f03728c8c60594e7590bc14d39afe90fee552
Alvaro Herrera <alvherre@alvh.no-ip.org>
2015-02-10 18:50:44 -0300

    deparse-test: add pg_regress --dbname-deparse option

--- src/test/regress/pg_regress_main.c
+++ src/test/regress/pg_regress_main.c
@@ -88,6 +88,7 @@ psql_init(int argc, char **argv)
 {
 	/* set default regression database name */
 	add_stringlist_item(&dblist, "regression");
+	strcpy(deparse_test_db, "regression_deparse");
 }
 
 int
