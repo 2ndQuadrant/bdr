6fbffe2f1f207e176210c7c34e8333ccbabc630b
Alvaro Herrera <alvherre@alvh.no-ip.org>
2015-03-24 18:55:25 -0300

    fix copyright year

--- src/backend/tcop/deparse_utility.c
+++ src/backend/tcop/deparse_utility.c
@@ -4,7 +4,7 @@
  *	  Functions to convert utility commands to machine-parseable
  *	  representation
  *
- * Portions Copyright (c) 1996-2012, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1996-2015, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
  * NOTES
