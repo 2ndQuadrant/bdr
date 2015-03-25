1415d342e9f6136179671d04ab461e76783f0f86
Petr Jelinek <pjmodos@pjmodos.net>
2015-03-25 21:53:53 +0100

    Fix PK detection on inherited tables.
    We don't allow UPDATEs or DELETEs on tables without primary key but the
    code for detecting if table has primary key didn't work correctly when
    the UPDATE/DELETE was run on parent table which had primary key but the
    command has propagated to an inherited table didn't have primary key.
    
    In passing, fix the primary key detection for a less common use case:
    WITH foo AS ( UPDATE table_without_pk ... RETURNING * )
    INSERT INTO table_with_pk SELECT * FROM foo;

--- sql/dml/missing_pk.sql
+++ sql/dml/missing_pk.sql
@@ -5,7 +5,8 @@ SELECT * FROM public.bdr_regress_variables()
 \c :writedb1
 
 SELECT bdr.bdr_replicate_ddl_command($$
-	CREATE TABLE public.bdr_missing_pk(a serial);
+	CREATE TABLE public.bdr_missing_pk_parent(a serial PRIMARY KEY);
+	CREATE TABLE public.bdr_missing_pk(a serial) INHERITS (public.bdr_missing_pk_parent);
 	CREATE VIEW public.bdr_missing_pk_view AS SELECT * FROM public.bdr_missing_pk;
 $$);
 
@@ -18,6 +19,9 @@ SELECT * FROM bdr_missing_pk;
 \c :writedb2
 UPDATE bdr_missing_pk SET a = 1;
 DELETE FROM bdr_missing_pk WHERE a = 1;
+
+UPDATE bdr_missing_pk_parent SET a = 1;
+DELETE FROM bdr_missing_pk_parent WHERE a = 1;
 
 WITH foo AS (
 	UPDATE bdr_missing_pk SET a = 1 WHERE a > 1 RETURNING a
@@ -37,6 +41,15 @@ WITH foo AS (
 WITH foo AS (
 	DELETE FROM bdr_missing_pk_view RETURNING a
 ) SELECT * FROM foo;
+
+WITH foo AS (
+	UPDATE bdr_missing_pk SET a = 1 RETURNING *
+) INSERT INTO bdr_missing_pk SELECT * FROM foo;
+
+WITH foo AS (
+	DELETE FROM bdr_missing_pk_view RETURNING a
+) INSERT INTO bdr_missing_pk SELECT * FROM foo;
+
 
 -- success again
 TRUNCATE bdr_missing_pk;
