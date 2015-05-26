d5f34c8782ed965c9e240dbd6fcb8339296692fe
Craig Ringer <craig@2ndquadrant.com>
2015-05-26 17:51:55 +0800

    Add bdr.bdr_apply_is_paused() information function
    This makes it possible to see whether replay is paused on
    a node.
    
    Also adds some tests for pause/resume.
    
    Closes #59

--- extsql/bdr--0.9.0.4--0.9.0.5.sql
+++ extsql/bdr--0.9.0.4--0.9.0.5.sql
@@ -9,6 +9,8 @@ SET bdr.skip_ddl_replication = true;
 ALTER FUNCTION bdr.table_set_replication_sets(p_relation regclass, p_sets text[])
 SET bdr.permit_unsafe_ddl_commands = true;
 
+CREATE OR REPLACE FUNCTION bdr.bdr_apply_is_paused()
+RETURNS boolean LANGUAGE c AS 'MODULE_PATHNAME';
 
 RESET bdr.permit_unsafe_ddl_commands;
 RESET bdr.skip_ddl_replication;
---
+++ sql/pause.sql
@@ -0,0 +1,44 @@
+\ccc regression
+
+SELECT bdr.bdr_apply_is_paused();
+
+SELECT bdr.bdr_replicate_ddl_command('CREATE TABLE public.pause_test(x text primary key);');
+INSERT INTO pause_test(x) VALUES ('before pause');
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+
+\cc postgres
+
+SELECT bdr.bdr_apply_is_paused();
+SELECT bdr.bdr_apply_pause();
+SELECT bdr.bdr_apply_is_paused();
+-- It's necessary to wait for a latch timeout on apply workers
+-- until bdr_apply_pause gets taught to set their latches.
+SELECT pg_sleep(6);
+
+\ccc regression
+
+INSERT INTO pause_test(x) VALUES ('after pause before resume');
+
+\cc postgres
+
+-- Give more time for a row to replicate if it's going to
+-- (it shouldn't)
+SELECT pg_sleep(1);
+
+-- Pause state is preserved across sessions
+SELECT bdr.bdr_apply_is_paused();
+
+-- Must not see row from after pause
+SELECT x FROM pause_test;
+
+SELECT bdr.bdr_apply_resume();
+
+\ccc regression
+
+INSERT INTO pause_test(x) VALUES ('after resume');
+SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
+
+\cc postgres
+
+-- Must see all three rows
+SELECT x FROM pause_test;
