38f63ce7cfc404ed4dcd09b8fd95ab82a773abd8
Petr Jelinek <pjmodos@pjmodos.net>
2015-06-01 22:15:01 +0200

    Add missing files for previous commit

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -63,6 +63,9 @@ DROP EXTENSION bdr;
 CREATE EXTENSION bdr VERSION '0.10.0.2';
 DROP EXTENSION bdr;
 
+CREATE EXTENSION bdr VERSION '0.10.0.3';
+DROP EXTENSION bdr;
+
 -- evolve version one by one from the oldest to the newest one
 CREATE EXTENSION bdr VERSION '0.8.0';
 ALTER EXTENSION bdr UPDATE TO '0.8.0.1';
@@ -82,6 +85,7 @@ ALTER EXTENSION bdr UPDATE TO '0.9.1.0';
 ALTER EXTENSION bdr UPDATE TO '0.10.0.0';
 ALTER EXTENSION bdr UPDATE TO '0.10.0.1';
 ALTER EXTENSION bdr UPDATE TO '0.10.0.2';
+ALTER EXTENSION bdr UPDATE TO '0.10.0.3';
 
 -- Should never have to do anything: You missed adding the new version above.
 ALTER EXTENSION bdr UPDATE;
