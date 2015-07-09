096d7aa00064a2474bb20106f13decc6b19f488a
Ian Barwick <ian@2ndquadrant.com>
2015-07-09 15:30:24 +0900

    Only install bdr.bdr_internal_sequence_reset_cache() in BDR mode
    bdr_seq.c not compiled for UDR; attempting to create this function
    in UDR mode prevents the extension from being installed.

--- extsql/bdr--0.10.0.5--0.10.0.6.sql
+++ extsql/bdr--0.10.0.5--0.10.0.6.sql
@@ -2,8 +2,13 @@ SET LOCAL search_path = bdr;
 SET bdr.permit_unsafe_ddl_commands = true;
 SET bdr.skip_ddl_replication = true;
 
-CREATE OR REPLACE FUNCTION bdr.bdr_internal_sequence_reset_cache(seq regclass)
-RETURNS void LANGUAGE c AS 'MODULE_PATHNAME' STRICT;
+DO $$
+BEGIN
+  IF bdr.bdr_variant() = 'BDR' THEN
+    CREATE OR REPLACE FUNCTION bdr.bdr_internal_sequence_reset_cache(seq regclass)
+    RETURNS void LANGUAGE c AS 'MODULE_PATHNAME' STRICT;
+  END IF;
+END$$;
 
 RESET bdr.permit_unsafe_ddl_commands;
 RESET bdr.skip_ddl_replication;
