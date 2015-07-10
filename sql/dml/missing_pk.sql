818b0724fb95710c90bea23947b16488a0ed478b
Petr Jelinek <petr@2ndquadrant.com>
2015-07-10 16:36:28 +0200

    Fix the missing_pk test to be user independent

--- sql/dml/missing_pk.sql
+++ sql/dml/missing_pk.sql
@@ -67,12 +67,28 @@ UPDATE pg_class
 SET relname = 'bdr_missing_pk_renamed'
 WHERE relname = 'bdr_missing_pk';
 
-\dt bdr_missing_pk*
+SELECT n.nspname as "Schema",
+  c.relname as "Name"
+FROM pg_catalog.pg_class c
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE c.relkind = 'r'
+  AND n.nspname !~ '^pg_toast'
+  AND c.relname ~ '^(bdr_missing_pk.*)$'
+  AND pg_catalog.pg_table_is_visible(c.oid)
+ORDER BY 1,2;
 SELECT pg_xlog_wait_remote_apply(pg_current_xlog_location(), 0);
 
 \c :readdb2
 -- The catalog change should not replicate
-\dt bdr_missing_pk*
+SELECT n.nspname as "Schema",
+  c.relname as "Name"
+FROM pg_catalog.pg_class c
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace
+WHERE c.relkind = 'r'
+  AND n.nspname !~ '^pg_toast'
+  AND c.relname ~ '^(bdr_missing_pk.*)$'
+  AND pg_catalog.pg_table_is_visible(c.oid)
+ORDER BY 1,2;
 
 \c :writedb1
 UPDATE pg_class
