c8b46e34836df9a1a213929b6c079b0add5b20c9
Craig Ringer <craig@2ndquadrant.com>
2016-08-11 12:29:31 +0800

    Tool to push docs

---
+++ doc/publish.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e -u
+
+rev=$(awk '/^#define BDR_VERSION / { match($3,"\"([0-9]+\\.[0-9]+)\\.[0-9]+",a); print a[1] }' ../bdr_version.h)
+
+MAKE=${MAKE:-make}
+DOCHOST="bdr-project.org"
+DOCDIR="/var/www_bdr-project.org/docs/"
+
+if [ -z "$rev" ]; then
+  echo "Couldn't determine version"
+  exit 1
+else
+  echo "Building docs for bdr-plugin $rev"
+fi
+
+$MAKE clean
+rm -rf html
+$MAKE JADEFLAGS="-V website-build"
+
+echo -n "Uploading... "
+RSYNC_RSH="ssh -o StrictHostKeyChecking=no" rsync \
+  -r --delete html/ \
+  $DOCHOST:$DOCDIR/$rev/
+echo "done"
