2a50974909ca4f12b6de9f7cb8dd694129011eec
Craig Ringer <craig@2ndquadrant.com>
2017-01-10 14:01:31 +0800

    Allow some ALTER TABLE subcommands to take the weaker DDL lock
    Allow a few ALTER TABLE subcommands to take a weaker DDL lock that does not
    block writes, only concurrent DDL.
    
    Currently this set is:
    
      CLUSTER ON
      SET WITHOUT CLUSTER
      OWNER TO
      SET STORAGE
      ENABLE TRIGGER
      DISABLE TRIGGER
      ENABLE USER TRIGGERS
      DISABLE USER TRIGGERS
    
    but more can be added later.

--- bdr_commandfilter.c
+++ bdr_commandfilter.c
@@ -172,7 +172,8 @@ filter_CreateStmt(Node *parsetree,
 static void
 filter_AlterTableStmt(Node *parsetree,
 					  char *completionTag,
-					  const char *queryString)
+					  const char *queryString,
+					  BDRLockType *lock_type)
 {
 	AlterTableStmt *astmt;
 	ListCell   *cell,
@@ -267,13 +268,15 @@ filter_AlterTableStmt(Node *parsetree,
 
 				case AT_ClusterOn:		/* CLUSTER ON */
 				case AT_DropCluster:	/* SET WITHOUT CLUSTER */
+				case AT_ChangeOwner:
+				case AT_SetStorage:
+					lock_type = BDR_LOCK_DDL;
+					break;
 
 				case AT_SetRelOptions:	/* SET (...) */
 				case AT_ResetRelOptions:		/* RESET (...) */
 				case AT_ReplaceRelOptions:		/* replace reloption list */
 				case AT_ReplicaIdentity:
-				case AT_ChangeOwner:
-				case AT_SetStorage:
 					break;
 
 				case AT_DropConstraint:
@@ -337,13 +340,25 @@ filter_AlterTableStmt(Node *parsetree,
 					break;
 
 				case AT_EnableTrig:
-				case AT_EnableAlwaysTrig:
-				case AT_EnableReplicaTrig:
 				case AT_DisableTrig:
-				case AT_EnableTrigAll:
-				case AT_DisableTrigAll:
 				case AT_EnableTrigUser:
 				case AT_DisableTrigUser:
+					/*
+					 * It's safe to ALTER TABLE ... ENABLE|DISABLE TRIGGER
+					 * without blocking concurrent writes.
+					 */
+					lock_type = BDR_LOCK_DDL;
+					break;
+
+				case AT_EnableAlwaysTrig:
+				case AT_EnableReplicaTrig:
+				case AT_EnableTrigAll:
+				case AT_DisableTrigAll:
+					/*
+					 * Since we might fire replica triggers later and that
+					 * could affect replication, continue to take a write-lock
+					 * for them.
+					 */
 					break;
 
 				case AT_EnableRule:
@@ -952,7 +967,7 @@ bdr_commandfilter(Node *parsetree,
 			break;
 
 		case T_AlterTableStmt:
-			filter_AlterTableStmt(parsetree, completionTag, queryString);
+			filter_AlterTableStmt(parsetree, completionTag, queryString, &lock_type);
 			break;
 
 		case T_AlterDomainStmt:
