d4b160a2670e80b0353b9b806bb4b8a88eb3c413
dgrowley@gmail.com <dgrowley@gmail.com>
2018-05-18 15:41:40 +1200

    Update version to 1.0.6

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -114,6 +114,9 @@ DROP EXTENSION bdr;
 CREATE EXTENSION bdr VERSION '1.0.5.0';
 DROP EXTENSION bdr;
 
+CREATE EXTENSION bdr VERSION '1.0.6.0';
+DROP EXTENSION bdr;
+
 -- evolve version one by one from the oldest to the newest one
 CREATE EXTENSION bdr VERSION '0.8.0';
 ALTER EXTENSION bdr UPDATE TO '0.8.0.1';
@@ -178,6 +181,7 @@ ALTER EXTENSION bdr UPDATE TO '1.0.3.0';
 ALTER EXTENSION bdr UPDATE TO '1.0.3.1';
 ALTER EXTENSION bdr UPDATE TO '1.0.4.0';
 ALTER EXTENSION bdr UPDATE TO '1.0.5.0';
+ALTER EXTENSION bdr UPDATE TO '1.0.6.0';
 
 -- Should never have to do anything: You missed adding the new version above.
 ALTER EXTENSION bdr UPDATE;
